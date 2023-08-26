07cc4d158af70931502cc2fd4e69d478f21fe378
Greg Griffith <griffith_g@hotmail.com>
2023-08-26 16:19:02 -0400

    added specification  section

--- website/src/app_components/specs.jsx
+++ website/src/app_components/specs.jsx
@@ -1,8 +1,39 @@
 export default function Specs() {
     return(
         <div>
-            <p>SPECS</p>
-            <p>specs</p>
+            <div className="text-lg text-center mb-3">SPECIFICATIONS</div>            
+            <div className="text-center space-y-3">
+                <div>
+                    <p>DASHBOARD</p>
+                    <p>Controller - Arduino Nano MCU
+                        <br/>Display Screen - 16x2 LCD  
+                        <br/>Rev Limiter - two-step with launch control
+                        <br/>Launch Control - WOT launch ability
+                        <br/>Shift/Warning light - LED light sequence
+                        <br/>Tachometer input - hall sensor
+                        <br/>Speedometer input - hall sensor
+                        <br/>Odometer - speed based calculation
+                        <br/>Hour Meter - engine on/off detection
+                    </p>
+                </div>
+                <div>
+                    <p>CHASSIS</p>
+                    <p>The chassis was made from random bits and pieces of metal I could attain cheaply or freely, mainly 1" square tubing and 1" x 2" tubing. It was stick welded using 6013 rod.</p>
+                </div>
+                <div>
+                    <p>ENGINE</p>
+                    <p>The original engine was a Honda 270GX 9HP utility engine, which was taken from a gas powered air-compressor. Eventually this was upgraded to a beastly 6-speed motorcycle engine from Honda CBR125. </p>
+                </div>
+                <div>
+                    <p>SUSPENSION</p>
+                    <p>Although this entire project can be considered &quot;unengineered&quot;, the suspension takes the cake in this department and 4 cool looking (red) springs where purchased. The front set was later replaced with a stiffer pair.</p>
+                </div>
+                <div>
+                    <p>BRAKES</p>
+                    <p>Initially the go kart was fitted with dual rear disc brakes originall from an italian made pocket bike. Once the CBR engine was installed, the CBR rear brake system was also installed on the go-kart.</p>
+                </div>
+
+            </div>
         </div>
     )
 }
