af669ed2ba5451de8ebb48a8a09c4bf167950a0a
Greg Griffith <griffith_g@hotmail.com>
2023-08-27 06:36:53 -0400

    troubleshooting deployment

--- website/src/app_components/gallery.jsx
+++ website/src/app_components/gallery.jsx
@@ -22,27 +22,27 @@ export default function Gallery() {
                             // navigation
                             pagination={{ clickable: true }}
                         >
-                            <SwiperSlide><div className='flex flex-col pb-5'><img src='/images/build0.jpg' className='h-64 mx-auto'></img>Slide 1</div></SwiperSlide>
-                            <SwiperSlide><img src='/images/build1.jpg' className='h-64 mx-auto'></img>Slide 2</SwiperSlide>
-                            <SwiperSlide><img src='/images/build2.jpg' className='h-64 mx-auto'></img>Slide 3</SwiperSlide>
-                            <SwiperSlide><img src='/images/build3.jpg' className='h-64 mx-auto'></img>Slide 4</SwiperSlide>
-                            <SwiperSlide><img src='/images/build4.jpg' className='h-64 mx-auto'></img>Slide 5</SwiperSlide>
-                            <SwiperSlide><img src='/images/build5.jpg' className='h-64 mx-auto'></img>Slide 6</SwiperSlide>
-                            <SwiperSlide><img src='/images/build6.jpg' className='h-64 mx-auto'></img>Slide 6</SwiperSlide>
-                            <SwiperSlide><img src='/images/build7.jpg' className='h-64 mx-auto'></img>Slide 7</SwiperSlide>
-                            <SwiperSlide><img src='/images/build8.jpg' className='h-64 mx-auto'></img>Slide 8</SwiperSlide>
-                            <SwiperSlide><img src='/images/build9.jpg' className='h-64 mx-auto'></img>Slide 9</SwiperSlide>
-                            <SwiperSlide><img src='/images/build10.jpg' className='h-64 mx-auto'></img>Slide 10</SwiperSlide>
-                            <SwiperSlide><img src='/images/build11.jpg' className='h-64 mx-auto'></img>Slide 11</SwiperSlide>
-                            <SwiperSlide><img src='/images/build12.jpg' className='h-64 mx-auto'></img>Slide 12</SwiperSlide>
-                            <SwiperSlide><img src='/images/build13.jpg' className='h-64 mx-auto'></img>Slide 13</SwiperSlide>
-                            <SwiperSlide><img src='/images/build14.jpg' className='h-64 mx-auto'></img>Slide 14</SwiperSlide>
-                            <SwiperSlide><img src='/images/build15.jpg' className='h-64 mx-auto'></img>Slide 15</SwiperSlide>
-                            <SwiperSlide><img src='/images/build16.jpg' className='h-64 mx-auto'></img>Slide 16</SwiperSlide>
-                            <SwiperSlide><img src='/images/build17.jpg' className='h-64 mx-auto'></img>Slide 17</SwiperSlide>
-                            <SwiperSlide><img src='/images/build18.jpg' className='h-64 mx-auto'></img>Slide 18</SwiperSlide>
-                            <SwiperSlide><img src='/images/build19.jpg' className='h-64 mx-auto'></img>Slide 19</SwiperSlide>
-                            <SwiperSlide><img src='/images/build20.jpg' className='h-64 mx-auto'></img>Slide 20</SwiperSlide>
+                            <SwiperSlide><div className='flex flex-col pb-5'><img src='../images/build0.jpg' className='h-64 mx-auto'></img>Slide 1</div></SwiperSlide>
+                            <SwiperSlide><img src='../images/build1.jpg' className='h-64 mx-auto'></img>Slide 2</SwiperSlide>
+                            <SwiperSlide><img src='../images/build2.jpg' className='h-64 mx-auto'></img>Slide 3</SwiperSlide>
+                            <SwiperSlide><img src='../images/build3.jpg' className='h-64 mx-auto'></img>Slide 4</SwiperSlide>
+                            <SwiperSlide><img src='../images/build4.jpg' className='h-64 mx-auto'></img>Slide 5</SwiperSlide>
+                            <SwiperSlide><img src='../images/build5.jpg' className='h-64 mx-auto'></img>Slide 6</SwiperSlide>
+                            <SwiperSlide><img src='../images/build6.jpg' className='h-64 mx-auto'></img>Slide 6</SwiperSlide>
+                            <SwiperSlide><img src='../images/build7.jpg' className='h-64 mx-auto'></img>Slide 7</SwiperSlide>
+                            <SwiperSlide><img src='../images/build8.jpg' className='h-64 mx-auto'></img>Slide 8</SwiperSlide>
+                            <SwiperSlide><img src='../images/build9.jpg' className='h-64 mx-auto'></img>Slide 9</SwiperSlide>
+                            <SwiperSlide><img src='../images/build10.jpg' className='h-64 mx-auto'></img>Slide 10</SwiperSlide>
+                            <SwiperSlide><img src='../images/build11.jpg' className='h-64 mx-auto'></img>Slide 11</SwiperSlide>
+                            <SwiperSlide><img src='../images/build12.jpg' className='h-64 mx-auto'></img>Slide 12</SwiperSlide>
+                            <SwiperSlide><img src='../images/build13.jpg' className='h-64 mx-auto'></img>Slide 13</SwiperSlide>
+                            <SwiperSlide><img src='../images/build14.jpg' className='h-64 mx-auto'></img>Slide 14</SwiperSlide>
+                            <SwiperSlide><img src='../images/build15.jpg' className='h-64 mx-auto'></img>Slide 15</SwiperSlide>
+                            <SwiperSlide><img src='../images/build16.jpg' className='h-64 mx-auto'></img>Slide 16</SwiperSlide>
+                            <SwiperSlide><img src='../images/build17.jpg' className='h-64 mx-auto'></img>Slide 17</SwiperSlide>
+                            <SwiperSlide><img src='../images/build18.jpg' className='h-64 mx-auto'></img>Slide 18</SwiperSlide>
+                            <SwiperSlide><img src='../images/build19.jpg' className='h-64 mx-auto'></img>Slide 19</SwiperSlide>
+                            <SwiperSlide><img src='../images/build20.jpg' className='h-64 mx-auto'></img>Slide 20</SwiperSlide>
                         </Swiper>
 
                 </div>
